afb76bac805581031cb7189ae3d16b8fa9963807
mvp <42063444+ke3peR007@users.noreply.github.com>
2023-08-30 17:41:27 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-# Load Gtk
-import gi
-gi.require_version('Gtk', '4.0')
-from gi.repository import Gtk
+"""
+Diary app
+"""
+from tkinter import Tk
+from tkinter import ttk
 
-# When the application is launched…
-def on_activate(app):
-    # … create a new window…
-    win = Gtk.ApplicationWindow(application=app)
-    # … with a button in it…
-    btn = Gtk.Button(label='Hello, World!')
-    # … which closes the window when clicked
-    btn.connect('clicked', lambda x: win.close())
-    win.set_child(btn)
-    win.present()
-
-# Create a new application
-app = Gtk.Application(application_id='com.example.GtkApplication')
-app.connect('activate', on_activate)
-
-# Run the application
-app.run(None)
+long_list = [
+    "this",
+    "list",
+    "contains",
+    "too",
+    "many",
+    "elements",
+    "for",
+    "one",
+    "line",
+]
+for i in long_list:
+    print(i)
+root = Tk()
+ttk.Button(root, text="Hello World").grid()
+root.mainloop()
